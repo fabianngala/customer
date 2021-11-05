49a4962218efda720d65c757f0e3d357bf81be95
fabian <fabbianngala@gmail.com>
2021-11-05 17:44:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,144 @@
+const { Elarian } = require("elarian")
+
+const client = new Elarian({
+    orgId: 'el_org_eu_f0FTOq',
+    appId: 'el_app_Erdeem',
+    apiKey: 'el_k_test_91fc7d8e81e1416e36f06eac06d83ea214f41ecfb2b98a6d06b4cfb623332264'
+});
+​
+client
+    .on('error', (err) => console.error(err))
+    .on('connected', onConnected)
+    .connect();
+
+async function onConnected() {
+    // Ready to interact with customers
+}
+const { Elarian } = require("elarian")
+
+const client = new Elarian({
+    orgId: 'el_org_eu_f0FTOq',
+    appId: 'el_app_Erdeem',
+    apiKey: 'el_k_test_91fc7d8e81e1416e36f06eac06d83ea214f41ecfb2b98a6d06b4cfb623332264'
+});
+​
+client
+    .on('error', (err) => console.error(err))
+    .on('connected', onConnected)
+    .connect();
+
+async function onConnected() {
+    // Ready to interact with customers
+}
+const { Elarian } = require("elarian")
+
+const client = new Elarian({
+    orgId: 'el_org_eu_f0FTOq',
+    appId: 'el_app_Erdeem',
+    apiKey: 'el_k_test_91fc7d8e81e1416e36f06eac06d83ea214f41ecfb2b98a6d06b4cfb623332264'
+});
+​
+client
+    .on('error', (err) => console.error(err))
+    .on('connected', onConnected)
+    .connect();
+
+async function onConnected() {
+    // Ready to interact with customers
+}
+const { Elarian } = require("elarian")
+
+const client = new Elarian({
+    orgId: 'el_org_eu_f0FTOq',
+    appId: 'el_app_Erdeem',
+    apiKey: 'el_k_test_91fc7d8e81e1416e36f06eac06d83ea214f41ecfb2b98a6d06b4cfb623332264'
+});
+​
+client
+    .on('error', (err) => console.error(err))
+    .on('connected', onConnected)
+    .connect();
+
+async function onConnected() {
+    // Ready to interact with customers
+}
+const { Elarian } = require("elarian")
+
+const client = new Elarian({
+    orgId: 'el_org_eu_f0FTOq',
+    appId: 'el_app_Erdeem',
+    apiKey: 'el_k_test_91fc7d8e81e1416e36f06eac06d83ea214f41ecfb2b98a6d06b4cfb623332264'
+});
+​
+client
+    .on('error', (err) => console.error(err))
+    .on('connected', onConnected)
+    .connect();
+
+async function onConnected() {
+    // Ready to interact with customers
+}
+const { Elarian } = require("elarian")
+
+const client = new Elarian({
+    orgId: 'el_org_eu_f0FTOq',
+    appId: 'el_app_Erdeem',
+    apiKey: 'el_k_test_91fc7d8e81e1416e36f06eac06d83ea214f41ecfb2b98a6d06b4cfb623332264'
+});
+​
+client
+    .on('error', (err) => console.error(err))
+    .on('connected', onConnected)
+    .connect();
+
+async function onConnected() {
+    // Ready to interact with customers
+}
+const { Elarian } = require("elarian")
+
+const client = new Elarian({
+    orgId: 'el_org_eu_f0FTOq',
+    appId: 'el_app_Erdeem',
+    apiKey: 'el_k_test_91fc7d8e81e1416e36f06eac06d83ea214f41ecfb2b98a6d06b4cfb623332264'
+});
+​
+client
+    .on('error', (err) => console.error(err))
+    .on('connected', onConnected)
+    .connect();
+
+async function onConnected() {
+    // Ready to interact with customers
+}
+const { Elarian } = require("elarian")
+
+const client = new Elarian({
+    orgId: 'el_org_eu_f0FTOq',
+    appId: 'el_app_Erdeem',
+    apiKey: 'el_k_test_91fc7d8e81e1416e36f06eac06d83ea214f41ecfb2b98a6d06b4cfb623332264'
+});
+​
+client
+    .on('error', (err) => console.error(err))
+    .on('connected', onConnected)
+    .connect();
+
+async function onConnected() {
+    // Ready to interact with customers
+}
+const { Elarian } = require("elarian")
+
+const client = new Elarian({
+    orgId: 'el_org_eu_f0FTOq',
+    appId: 'el_app_Erdeem',
+    apiKey: 'el_k_test_91fc7d8e81e1416e36f06eac06d83ea214f41ecfb2b98a6d06b4cfb623332264'
+});
+​
+client
+    .on('error', (err) => console.error(err))
+    .on('connected', onConnected)
+    .connect();
+
+async function onConnected() {
+    // Ready to interact with customers
+}
